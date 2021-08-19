276e39216369f21437e664567529d6332fa5d9d5
Hector Lopez <hectorlopezp02@gmail.com>
2021-08-18 23:56:42 -0500

    Se factoriza metodo para crear admin por defecto

--- src/libs/initialSetup.js
+++ src/libs/initialSetup.js
@@ -1,4 +1,6 @@
 import Role from '../models/Role';
+import User from "../models/User";
+import bcrypt from "bcryptjs";
 
 export const createRoles= async () => {
     try {
@@ -9,8 +11,22 @@ export const createRoles= async () => {
             new Role({ name: "moderator" }).save(),
             new Role({ name: "admin" }).save(),
         ]);
-        console.log(values);
+    console.log('roles creados');
     } catch (error) {
         console.error(error);
     }
-}
+};
+
+export const createAdmin = async () => {
+    const user = await User.findOne({ email: "admin@localhost" });
+    const roles = await Role.find({ name: { $in: ["admin", "moderator"] } });
+    if (!user) {
+      await User.create({
+        username: "admin",
+        email: "admin@localhost",
+        password: await bcrypt.hash("admin", 10),
+        roles: roles.map((role) => role._id),
+      });
+      console.log('Usuario Admin Creado');
+    }
+  };
