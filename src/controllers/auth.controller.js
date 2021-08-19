4418c5dd4369055df994a0e84ba350d54210377e
Hector Lopez <hectorlopezp02@gmail.com>
2021-08-18 23:54:47 -0500

    Se refactoriza auth y product controller, se crea user controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,37 +4,51 @@ import config from '../config';
 import Role from '../models/Role';
 
 export const signUp = async (req, res) => {
-    const { username, password, email, roles } = req.body;
-    const newUser = new User({
-        username,
-        email,
-        password: await User.encryptPassword(password)
-    });
+    try {
+        const { username, password, email, roles } = req.body;
+        const newUser = new User({
+            username,
+            email,
+            password: await User.encryptPassword(password)
+        });
 
-    if (roles) {
-        const foundRoles = await Role.find({name: {$in: roles}})
-        newUser.roles = foundRoles.map(role => role._id);
-    } else {
-        const role = await Role.findOne({name: "user"});
-        newUser.roles = [role.id];
+        if (req.body.roles) {
+            const foundRoles = await Role.find({name: {$in: roles}})
+            newUser.roles = foundRoles.map(role => role._id);
+        } else {
+            const role = await Role.findOne({name: 'user'});
+            newUser.roles = [role._id];
+        }
+        
+        const savedUser = await newUser.save();
+        const token = jwt.sign({id: savedUser._id}, config.SECRET, {
+            expiresIn: 1800//30 min
+        });
+        res.status(200).json({token});
+    } catch (error) {
+        console.log('error en el SignUp')
+        return res.status(500).json(error);
     }
-    
-    const savedUser = await newUser.save();
-    const token = jwt.sign({id: savedUser._id}, config.SECRET, {
-        expiresIn: 1200//20 min
-    });
-    res.status(200).json({token});
 }
 
 export const signIn = async (req, res) => {
-    const userFound = await User.findOne({email: req.body.email}).populate("roles");
-    if (!userFound) return res.status(400).json({message:'Usuario no encontrado'});
-    const matchPassword = await User.comparePassword(req.body.password, userFound.password);
-    if (!matchPassword) return res.status(401).json({token: null, message: 'Pass Invalido'});
-    const token = jwt.sign({id: userFound._id}, config.SECRET, {
-        expiresIn: 1200//20 min
-    });
-    console.log(userFound);
-    res.json({token:""})
+    try {
+        const userFound = await User.findOne({ email: req.body.email }).populate("roles");
 
+        if (!userFound) return res.status(400).json({message:'Usuario no encontrado'});
+        
+        const matchPassword = await User.comparePassword(req.body.password, userFound.password);
+        console.log(req.body.password);
+        console.log(userFound.password);
+        if (!matchPassword) return res.status(401).json({token: null, message: 'Pass Invalido'});
+        
+        const token = jwt.sign({id: userFound._id}, config.SECRET, {
+            expiresIn: 1800//30 min
+        });
+        res.json({token})
+    } catch (error) {
+        console.log('error en SingIn');
+        return res.status(500).json(error);
+    }
+    
 }
--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -3,14 +3,20 @@ import Product from "../models/Product";
 
 export const createProduct = async(req, res) => {
     const {name, category, price, imgUrl} = req.body;
-    const newProduct = new Product({name, category, price, imgUrl});
-    const productSaved = await newProduct.save();
-    res.status(201).json(productSaved);
+    try {
+        const newProduct = new Product({name, category, price, imgUrl});
+        const productSaved = await newProduct.save();
+        res.status(201).json(productSaved);
+    } catch (error) {
+        console.log('Err: crear producto');
+        return res.status(500).json(error);
+    }
+    
 }
 
 export const getProducts = async(req, res) => {
     const products = await Product.find();
-    res.json(products);
+    res.status(200).json(products);
 }
 
 export const getProductById = async(req, res) => {
@@ -22,7 +28,7 @@ export const updateProductById = async(req, res) => {
     const updateProduct = await Product.findByIdAndUpdate(req.params.productId, req.body, {
         new: true
     })
-    res.status(200).json(updateProduct);
+    res.status(204).json(updateProduct);
 }
 export const deleteProductById = async(req, res) => {
     const deletedProduct = await Product.findByIdAndDelete(req.params.productId);
